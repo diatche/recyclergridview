e3426ecafd0b4f23cbd9b7305e7ef8a58a9b3f88
Pavel Diatchenko <diatche@gmail.com>
2020-10-08 09:30:24 +1300

    Fixed configuration to support expo and unit tests.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    // transform: { '^.+\\.ts?$': 'ts-jest' },
+    // testEnvironment: 'node',
+    // testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    preset: 'ts-jest',
+    // globals: {
+    //     'ts-jest': {
+    //         tsConfig: {
+    //             types: [],
+    //         },
+    //     },
+    // },
 };
